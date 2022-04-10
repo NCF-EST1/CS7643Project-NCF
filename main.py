d68b90f227339e9d6814e15713292552ad452653
Lyrica <lyric9771@gmail.com>
2022-04-10 00:36:48 -0400

    add running GMF, NCF_GMF

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ from models.NCF import NCF
 from models.GMF import GMF
 from models.NCF_GMF import NCF_GMF
 
-def training(epoch, train_loader, model, optimizer, criterion):
+def training(epoch, train_loader, model, optimizer, criterion, device):
+    model.train()
     losses = []
     for idx, (user, item, target) in enumerate(train_loader):
+        user = user.to(device)
+        item = item.to(device)
+        target = target.to(device)
+
         out  = model(user, item)
         target = target.float()
         loss = criterion(out.view(-1), target)
@@ -28,7 +33,7 @@ def training(epoch, train_loader, model, optimizer, criterion):
         if idx % 100 == 0:
             print(('Training Epoch: {0} {1}/{2} Loss: {3} AvgLoss: {4}').format(epoch, idx, len(train_loader), loss, mean(losses)))
 
-def validate_single(i, ratings, negatives, model, criterion):
+def validate_single(i, ratings, negatives, model, criterion, device):
     user = ratings[i][0]
     item = ratings[i][1]
     items = negatives[i]
@@ -37,7 +42,10 @@ def validate_single(i, ratings, negatives, model, criterion):
     user_in = torch.LongTensor(user_in)
     items = torch.LongTensor(items)
 
+    model.eval()
     with torch.no_grad():
+        user_in = user_in.to(device)
+        items = items.to(device)
         out = model(user_in, items)
     item_pred = {}
     for i in range(len(items)):
@@ -50,10 +58,10 @@ def validate_single(i, ratings, negatives, model, criterion):
         ndcg = math.log(2)/math.log(idx+2)
     return hr, ndcg
 
-def validate(epoch, test_ratings, test_negatives, model, criterion):
+def validate(epoch, test_ratings, test_negatives, model, criterion, device):
     hrs, ndcgs = [], []
     for i in range(len(test_ratings)):
-        hr, ndcg = validate_single(i, test_ratings, test_negatives, model, criterion)
+        hr, ndcg = validate_single(i, test_ratings, test_negatives, model, criterion, device)
         hrs.append(hr)
         ndcgs.append(ndcg)
 
@@ -101,32 +109,43 @@ def main():
 
     train, testRatings, testNegatives = dataset.trainMatrix, dataset.testRatings, dataset.testNegatives
     num_users, num_items = train.shape
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("You are using device: %s" % device)
 
     if args.models:
         if args.models == 'NCF':
-            model = NCF(num_users, num_items, 8)
+            model = NCF(num_users, num_items, args.mlp_emb_size)
+        if args.models == 'GMF':
+            model = GMF(num_users, num_items, args.gmf_emb_size)
+        if args.models == 'NCF_GMF':
+            model = NCF_GMF(num_users, num_items, args.mlp_emb_size, args.gmf_emb_size)
+    model = model.to(device)
     print(model)
     
     criterion = nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
     best_model = None
-    best_hr = 0.0
+    best_hr, best_ndcg, best_epoch = 0.0, 0.0, 0
     for epoch in range(args.epochs):
-        user_input, item_input, labels = get_train_instances(train, 4)
+        user_input, item_input, labels = get_train_instances(train, args.num_negatives)
         ds = MyDataset(user_tensor=torch.LongTensor(user_input), item_tensor=torch.LongTensor(item_input), label_tensor=torch.LongTensor(labels))
-        train_loader = DataLoader(ds, batch_size=256, shuffle=True)
-        training(epoch, train_loader, model, optimizer, criterion)
-        hrs, ndcgs = validate(epoch, testRatings, testNegatives, model, criterion)
+        train_loader = DataLoader(ds, batch_size=args.batch_size, shuffle=True)
+        training(epoch, train_loader, model, optimizer, criterion, device)
+        hrs, ndcgs = validate(epoch, testRatings, testNegatives, model, criterion, device)
 
         print(("Validation Result Epoch: {0} HitRate: {1} Best HitRate: {2}").format(epoch, mean(hrs), best_hr))
 
         if mean(hrs) > best_hr:
             best_hr = mean(hrs)
+            best_ndcg = mean(ndcgs)
+            best_epoch = epoch
             best_model = copy.deepcopy(model)
+            if args.save_best:
+                torch.save(best_model.state_dict(), './saved_models/' + str(model.name) + '.pth')
 
-    if args.save_best:
-        torch.save(best_model.state_dict(), './saved_models/' + str(model.name) + '.pth')
+    print("Best Epoch: {0} HitRate: {1} Ndcg: {2}".format(best_epoch, best_hr, best_ndcg))
+
 
 if __name__ == '__main__':
     main()
--- models/NCF_GMF.py
+++ models/NCF_GMF.py
@@ -3,9 +3,44 @@ import torch.nn as nn
 
 class NCF_GMF(nn.Module):
 
-    def __init__(self):
+    def __init__(self, num_users, num_items, mlp_emb_size, gmf_emb_size):
         super(NCF_GMF, self).__init__()
         self.name = "NCF_GMF"
+        self.num_users = num_users
+        self.num_items = num_items
+        self.mlp_emb_size = mlp_emb_size
+        self.gmf_emb_size = gmf_emb_size
+
+        self.mlp_user_emb = nn.Embedding(self.num_users, self.mlp_emb_size)
+        self.mlp_item_emb = nn.Embedding(self.num_items, self.mlp_emb_size)
+        self.gmf_user_emb = nn.Embedding(self.num_users, self.gmf_emb_size)
+        self.gmf_item_emb = nn.Embedding(self.num_items, self.gmf_emb_size)
+
+        self.mlp_linear = nn.Sequential(
+            nn.Linear(self.mlp_emb_size * 2, 64),
+            # nn.Linear(64, 32),
+            # nn.Linear(32, 16),
+            # nn.Linear(16, 8),
+            nn.ReLU()
+        )
+
+        self.final = nn.Sequential(
+            nn.Linear(64 + self.gmf_emb_size, 1),
+            nn.Sigmoid()
+        )
+
     
-    def forward(self):
-        pass
+    def forward(self, users, items):
+        mlp_user_emb = self.mlp_user_emb(users)
+        mlp_item_emb = self.mlp_item_emb(items)
+        mlp_user_item = torch.cat([mlp_user_emb, mlp_item_emb], dim=-1)
+        mlp_output = self.mlp_linear(mlp_user_item)
+        
+        gmf_user_emb = self.gmf_user_emb(users)
+        gmf_item_emb = self.gmf_item_emb(items)
+        gmf_user_item = torch.mul(gmf_user_emb, gmf_item_emb)
+
+        mlp_gmf = torch.cat([mlp_output, gmf_user_item], dim=-1)
+        output = self.final(mlp_gmf)
+        return output
+
